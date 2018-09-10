b518bea97c464f17c14fca6d0317a4cfde400ec6
Emre <kklemre@gmail.com>
2018-09-11 00:46:26 +0300

    update
    update

--- _assets/js/macpoint.js
+++ _assets/js/macpoint.js
@@ -7747,27 +7747,71 @@
   return Swiper;
 
 })));
-;
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+;var macPoint = {
+  swipe: function(){
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    var mainSlide = new Swiper ('.slide-section .swiper-container', {
+      // Optional parameters
+      effect: 'fade',
+      slidesPerView: 1,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    mainSlide.on('slideChange', function () {
+      macPoint.animateRender();
+    });
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+    var middleSlide = new Swiper ('.slide-section-middle .swiper-container', {
+      // Optional parameters
+      effect: 'fade',
+      slidesPerView: 1,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next-2',
+        prevEl: '.swiper-button-prev-2',
+      },
+    });
 
-  console.log('test');
+
+  },
+  animateRender: function(){
+    $(".animated").each(function(){
+
+      $(".slide-section .animated").attr("class","animated");
+
+      var $this = $(this),
+          animate = $this.attr("data-animate-type"),
+          time = Number($this.attr("data-time"));
+          setTimeout(function(){
+            $this.addClass(animate);
+          }, time);
+
+    });
+  },
+  oneLine: function(){
+    $(document).on('click', '.slide-section a .next', function(e){
+      e.preventDefault();
+      $(".slide-section .swiper-button-next").trigger("click");
+    });
+    $(document).on('click', '.slide-section a .prev', function(e){
+      e.preventDefault();
+      $(".slide-section .swiper-button-prev").trigger("click");
+    });
+  },
+  subInit: function(){
+    this.swipe();
+    this.animateRender();
+    this.oneLine();
+  },
+  init: function(){
+    this.subInit();
+  },
+};
+
+$(document).ready(function(){
+  macPoint.init();
+});
